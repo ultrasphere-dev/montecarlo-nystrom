53ff29f433eb5fc1196c291d94c8085dab9ccd99
34j <34j.github@proton.me>
2025-10-13 15:06:35 +0900

    feat: add main feat

--- src/montecarlo_nystrom/cli.py
+++ src/montecarlo_nystrom/cli.py
@@ -10,7 +10,7 @@ app = App()
 
 
 @app.command
-def case(case_num: int, N: int = 2000) -> None:
+def case(case_num: int, N: int = 2000, M: int = 100) -> None:
     """Compute examples in the paper."""
     if 1 <= case_num <= 3:
 
@@ -39,6 +39,7 @@ def case(case_num: int, N: int = 2000) -> None:
             kernel=kernel,
             rhs=rhs,
             n=N,
+            n_mean=M,
         )
         z = zf(x)
         fig, ax = plt.subplots()
@@ -46,3 +47,33 @@ def case(case_num: int, N: int = 2000) -> None:
         ax.set_title(f"Case {case_num}")
         ax.legend()
         fig.savefig(f"case_{case_num}.png")
+    if case_num == 4:
+        import ultrasphere as us
+
+        k = 5
+        m = 10
+
+        def kernel(x: Array, y: Array, /) -> Array:
+            xp = array_namespace(x, y)
+            k_ = xp.asarray(k)
+            m_ = xp.asarray(m)
+            return (m_ - 1) * k_**2 * us.fundamental_solution(xp.asarray(2), x - y, k_)
+
+        def rhs(x: Array, /) -> Array:
+            xp = array_namespace(x)
+            return xp.exp(xp.asarray(1j * k) * x[..., 0])
+
+        def rho(n: int, /) -> Array:
+            return us.random_ball(us.create_polar(), shape=(n,), xp=np)
+
+        x = us.random_ball(us.create_polar(), shape=(N,), xp=np)
+        zf = montecarlo_nystrom(
+            random_samples=rho,
+            kernel=kernel,
+            rhs=rhs,
+            n=N,
+            n_mean=M,
+        )
+        z = zf(x)
+        fig, ax = plt.subplots()
+        ax.scatter(x[:, 0], x[:, 1], c=np.real(z), cmap="jet")
